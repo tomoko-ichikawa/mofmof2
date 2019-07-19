3e0f3e9c21570d807bf4bc4c8916abcb2691ec42
tomokoichikawa <takator@yahoo.co.jp>
2019-07-20 00:47:00 +0900

    バリデーション、フォント等設定

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,9 @@
 class Rental < ApplicationRecord
+    validates :property, presence: true
+    validates :rent, presence: true, numericality: { only_integer: true }, length: { minimum: 5 } 
+    validates :address, presence: true
+    validates :age, presence: true, numericality: { only_integer: true }
+    validates :remarks, presence: true
     has_many :stations, dependent: :destroy
     accepts_nested_attributes_for :stations
 end
